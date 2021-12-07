341f5bf0a56ad22fc526d207ef71c6699eaf7764
murilmestre <83371960+murilmestre@users.noreply.github.com>
2021-12-07 17:13:04 -0300

    Brincaderinha,gosto de apostar e fiz uma cauculadora de odds

--- ex07EstruturaForUdemy/src/Main.java
+++ ex07EstruturaForUdemy/src/Main.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n =sc.nextInt();
-        for(int i=1;i<=n;i++){
-            int segundo =i*i;
-            int terceiro = i*i*i;
-            System.out.printf("%d %d %d %n", i,segundo,terceiro);
-        }
+        System.out.print("---------------------------------------");
+        System.out.println("");
+        System.out.println("|                                     |");
+        System.out.println("");
+        System.out.println("|                                     |");
+        System.out.println("");
+        System.out.println("|                                     |");
+        System.out.println("");
+        System.out.println("|                                     |");
+        System.out.println("");
+        System.out.println("---------------------------------------");
     }
 }
